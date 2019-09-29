00cb64c1a99ec62a71b434c49c12e37040e46bcd
herve <herve.mignot@equancy.com>
2019-09-29 13:51:48 +0200

    Change extension import to match documentation

--- test_basicauth.py
+++ test_basicauth.py
@@ -2,7 +2,7 @@ import base64
 import unittest
 
 from flask import Flask
-from flask.ext.basicauth import BasicAuth
+from flaskbasicauth import BasicAuth
 
 
 class BasicAuthTestCase(unittest.TestCase):
